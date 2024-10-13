f28ea8713eeee743ebf791f1e60db80555210274
Diana Carolina Perez <dianaperezc10@gmail.com>
2024-10-13 12:11:40 -0500

    Pay Mode View

---
+++ Supermarket-mvp/Views/PayModeView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Supermarket_mvp.Views
+{
+    public partial class PayModeView : Form
+    {
+        private bool isEdit;
+        private bool isSuccessful;
+        private string message;
+        public PayModeView()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
